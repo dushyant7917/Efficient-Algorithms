5c21a0856ab3b723e251a6f16b44c1bc0d0cdc9a
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-10-09 15:00:48 +0530

    Update and rename B.c to prime_number.c

---
+++ prime_number.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int prime_num(int number)
+{
+    if(number==1)
+       return 0;
+    if(number==2)
+       return 1;
+    if(number==3)
+       return 1;
+    if(number%2==0)
+       return 0;
+    if(number%3==0)
+       return 0;
+
+    int i=5;
+    int w=2;
+
+    while(i*i<=number)
+     {
+        if(number%i==0)
+            return 0;
+
+        i+=w;
+        w=6-w;
+     }
+
+    return 1;
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    if(prime_num(num)==1)
+    printf("\n%d is a prime number.",num);
+    else
+    printf("\n%d is a not a prime number.",num);
+
+    return 0;
+}
