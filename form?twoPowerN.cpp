c2c993f9464fa9009a7083d15c15f6f444fe8d89
DUSHYANT SINGH <dushyant7917@gmail.com>
2016-10-21 18:38:49 +0530

    Create form?twoPowerN.cpp
    The given algorithm determines if the given number is of the form 2^n or not.

---
+++ form?twoPowerN.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool multipleTwo(int x)
+{
+    return (!(x & (x-1)));
+}
+
+int main()
+{
+int n;
+cin>>n;
+if(multipleTwo(n))
+cout<<n<<" is of the form 2^n";
+else
+cout<<n<<" is NOT of the form 2^n";
+return 0;
+}
