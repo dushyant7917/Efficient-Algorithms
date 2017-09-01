38784c1d1a134bd53afe11c45c728665a29d20a2
DUSHYANT SINGH <dushyant7917@gmail.com>
2017-09-01 22:57:37 +0530

    my prefered template for competitive coding in cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+#define loopi(a,b) for(ll i=a;i<b;i++)
+#define loopj(a,b) for(ll j=a;i<b;j++)
+
+int main(){
+
+
+  return 0;
+}
