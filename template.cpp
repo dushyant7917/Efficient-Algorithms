8debfdad889ef9ad8f5b56aef4e92af34ef7f30f
DUSHYANT SINGH <dushyant7917@gmail.com>
2019-01-06 08:09:14 +0530

    tabs=4

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@ using namespace std;
 typedef long long ll;
 typedef double dbl;
 #define fr(x,a,b) for(ll x=a;x<b;x++)
-#define rf(x,a,b) for(ll x=a;x>b;x--)
+#define rf(x,a,b) for(ll x=a;x>=b;x--)
 #define pii pair<ll,ll>
 #define fi first
 #define se second
@@ -16,22 +16,23 @@ typedef double dbl;
 #define gmin LLONG_MIN
 #define INF 2e9
 #define N 100001
-#define MAX(a,b,c) max(max(a,b),c)
-#define MIN(a,b,c) min(min(a,b),c)
-#define SZ(s) s.size()
+#define MAX3(a,b,c) max(max(a,b),c)
+#define MIN3(a,b,c) min(min(a,b),c)
+#define SZ(s) ((ll)s.size())
 #define MS(x,v) memset(x,v,sizeof(x))
 
 int main(){
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  ll t;
-  cin>>t;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  ll n;
-  while(t--){
-    cin>>n;
-  }
+    ll t;
+    cin>>t;
 
-  return 0;
+    ll n;
+    while(t--){
+        cin>>n;
+    }
+
+    return 0;
 }
